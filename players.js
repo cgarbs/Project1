e33f4edaa8eb184f86b4faf44561c0941233cfba
Christian <christian.garba@gmail.com>
2020-12-01 23:57:53 -0700

    base player movement on dice roll

--- players.js
+++ players.js
@@ -9,58 +9,61 @@ blueCorner1.appendChild(player);
 let place = 0;
 
 player.onclick = function() {
-    // here we def logic for whether player can move
     if(playerMove === 1) {
+        if(place + diceRoll < 24) {
+        place = place + diceRoll;
         positions[place].appendChild(player);
-        place++;
-        playerMove = (playerMove + 1) % 2;
+        console.log(place);
+        // place++;
+        // playerMove = (playerMove + 1);
+        }
     }
 }
 
-// Player 1 Piece2
-const player2 = document.createElement("button");
-player2.style.width = "50px";
-player2.style.height = "50px";
-player2.style.backgroundColor = "lightBlue";
+// // Player 1 Piece2
+// const player2 = document.createElement("button");
+// player2.style.width = "50px";
+// player2.style.height = "50px";
+// player2.style.backgroundColor = "lightBlue";
 
-blueCorner2.appendChild(player2);
+// blueCorner2.appendChild(player2);
 
-let place2 = 0;
+// let place2 = 0;
 
-player2.onclick = function() {
-    positions[place2].appendChild(player2);
-    place2++;
-}
+// player2.onclick = function() {
+//     positions[place2].appendChild(player2);
+//     place2++;
+// }
 
-// Player 1 Piece3
-const player3 = document.createElement("button");
-player3.style.width = "50px";
-player3.style.height = "50px";
-player3.style.backgroundColor = "lightBlue";
+// // Player 1 Piece3
+// const player3 = document.createElement("button");
+// player3.style.width = "50px";
+// player3.style.height = "50px";
+// player3.style.backgroundColor = "lightBlue";
 
-blueCorner3.appendChild(player3);
+// blueCorner3.appendChild(player3);
 
-let place3 = 0;
+// let place3 = 0;
 
-player3.onclick = function() {
-    positions[place3].appendChild(player3);
-    place3++;
-}
+// player3.onclick = function() {
+//     positions[place3].appendChild(player3);
+//     place3++;
+// }
 
-// Player 1 Piece4
-const player4 = document.createElement("button");
-player4.style.width = "50px";
-player4.style.height = "50px";
-player4.style.backgroundColor = "lightBlue";
+// // Player 1 Piece4
+// const player4 = document.createElement("button");
+// player4.style.width = "50px";
+// player4.style.height = "50px";
+// player4.style.backgroundColor = "lightBlue";
 
-blueCorner4.appendChild(player4);
+// blueCorner4.appendChild(player4);
 
-let place4 = 0;
+// let place4 = 0;
 
-player4.onclick = function() {
-    positions[place4].appendChild(player4);
-    place4++;
-}
+// player4.onclick = function() {
+//     positions[place4].appendChild(player4);
+//     place4++;
+// }
 
 
 
@@ -78,51 +81,54 @@ redCorner1.appendChild(redPlayer);
 let redPlace = 0;
 
 redPlayer.onclick = function() {
-    positions2[redPlace].appendChild(redPlayer);
-    redPlace++;
+    if(playerMove === 2) {
+        positions2[redPlace].appendChild(redPlayer);
+        redPlace++;
+        playerMove = (playerMove - 1);
+    }
 }
 
-// Player 2 Piece2
-const redPlayer2 = document.createElement("button");
-redPlayer2.style.width = "50px";
-redPlayer2.style.height = "50px";
-redPlayer2.style.backgroundColor = "pink";
+// // Player 2 Piece2
+// const redPlayer2 = document.createElement("button");
+// redPlayer2.style.width = "50px";
+// redPlayer2.style.height = "50px";
+// redPlayer2.style.backgroundColor = "pink";
 
-redCorner2.appendChild(redPlayer2);
+// redCorner2.appendChild(redPlayer2);
 
-let redPlace2 = 0;
+// let redPlace2 = 0;
 
-redPlayer2.onclick = function() {
-    positions2[redPlace2].appendChild(redPlayer2);
-    redPlace2++;
-}
+// redPlayer2.onclick = function() {
+//     positions2[redPlace2].appendChild(redPlayer2);
+//     redPlace2++;
+// }
 
-// Player 2 Piece3
-const redPlayer3 = document.createElement("button");
-redPlayer3.style.width = "50px";
-redPlayer3.style.height = "50px";
-redPlayer3.style.backgroundColor = "pink";
+// // Player 2 Piece3
+// const redPlayer3 = document.createElement("button");
+// redPlayer3.style.width = "50px";
+// redPlayer3.style.height = "50px";
+// redPlayer3.style.backgroundColor = "pink";
 
-redCorner3.appendChild(redPlayer3);
+// redCorner3.appendChild(redPlayer3);
 
-let redPlace3 = 0;
+// let redPlace3 = 0;
 
-redPlayer3.onclick = function() {
-    positions2[redPlace3].appendChild(redPlayer3);
-    redPlace3++;
-}
+// redPlayer3.onclick = function() {
+//     positions2[redPlace3].appendChild(redPlayer3);
+//     redPlace3++;
+// }
 
-// Player 2 Piece4
-const redPlayer4 = document.createElement("button");
-redPlayer4.style.width = "50px";
-redPlayer4.style.height = "50px";
-redPlayer4.style.backgroundColor = "pink";
+// // Player 2 Piece4
+// const redPlayer4 = document.createElement("button");
+// redPlayer4.style.width = "50px";
+// redPlayer4.style.height = "50px";
+// redPlayer4.style.backgroundColor = "pink";
 
-redCorner4.appendChild(redPlayer4);
+// redCorner4.appendChild(redPlayer4);
 
-let redPlace4 = 0;
+// let redPlace4 = 0;
 
-redPlayer4.onclick = function() {
-    positions2[redPlace4].appendChild(redPlayer4);
-    redPlace4++;
-}
+// redPlayer4.onclick = function() {
+//     positions2[redPlace4].appendChild(redPlayer4);
+//     redPlace4++;
+// }
